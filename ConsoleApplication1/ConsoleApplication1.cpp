c528655ec417522d2956a3fcb9f5032542f44419
Тарбаев Дмитрий <kuprum321@gmail.com>
2022-09-23 21:50:26 +0300

    Create ConsoleApplication1.cpp

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    int firstX, firstY, secondX ,secondY;
+    printf("Enter first coordinats:");
+    scanf_s("%d", &firstX); 
+    scanf_s("%d", &firstY);
+    
+    printf("Enter second coordinats:");
+    scanf_s(" %d ", &secondX);
+    scanf_s(" %d ", &secondY);
+    float distance = sqrt((firstX - secondX) * (firstX - secondX) + (firstY - secondY) * (firstY - secondY));
+    printf("The distance is %f", distance);
+
+    return 0;
+}
+
