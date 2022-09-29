5d2df42041198c16eafbbc6e036343ae383584d2
Дмитрий Тарбаев <112640209+Cupru-m@users.noreply.github.com>
2022-09-29 14:06:40 +0300

    Add files via upload

---
+++ Lab-2-task-2/Lab-2-task-2/Lab-2-task-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+int main()
+{
+	float  a, b = 0;
+	int n;
+	printf("enter integer N:");
+	scanf_s("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		a = 1.0 / i;
+		b = b + a;
+	}
+	printf("sum of numbers from 1 to 1/n = %f", b);
+	return 0;
+
+}
