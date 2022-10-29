5d13c9a472b20c978e1e72c84de6f3d92afec23f
KuPruM <kuprum321@gmail.com>
2022-10-29 11:06:46 +0300

    Update Lab-3-task-3.cpp

--- Lab-3-task-3/Lab-3-task-3/Lab-3-task-3.cpp
+++ Lab-3-task-3/Lab-3-task-3/Lab-3-task-3.cpp
@@ -1,71 +1,71 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-int safe_scanf( int number);
+int safe_scanf(int number);
 int input_mas(int* m, int size);
 void output_mas(int* m, int size);
 int main()
 {
-    int mas_1[100], mas_2[100];
-    int size_1 = 1 , size_2 = 1 ;
+    int mas_inc[100], mas_dec[100];
+    int size_inc = 1, size_dec = 1;
     printf("Imput the size of increasing array :");
-    size_1=safe_scanf(size_1);
+    size_inc = safe_scanf(size_inc);
     printf("\nImput the size of decreasing array:");
-   size_2=  safe_scanf(size_2);
-    while (size_1 <= 0 || size_2 <= 0 || size_1>100 || size_2>100)
+    size_dec = safe_scanf(size_dec);
+    while (size_inc <= 0 || size_dec <= 0 || size_inc > 100 || size_dec > 100)
     {
         printf("\nSizes must be > 0 and < 100 \n");
         rewind(stdin);
         printf("\nImput the size of increasing array :");
-        size_1=safe_scanf(size_1);
+        size_inc = safe_scanf(size_inc);
         printf("\nImput the size of decreasing array:");
-        size_2 = safe_scanf(size_2);
+        size_dec = safe_scanf(size_dec);
     }
+
     printf("Increasing array :\n");
-    input_mas(mas_1, size_1);
+    input_mas(mas_inc, size_inc);
     printf("Decreasing array:\n");
-    input_mas(mas_2, size_2);
-     int new_size = size_1 + size_2;
-     int new_mas[200];
-   
-        for (int i = 0, k = size_1 -1, j = 0; i < new_size;)
-        {           
-            
-            if (mas_2[j] > mas_1[k])
-            {
-                new_mas[i] = mas_2[j];
-                if (j < size_2)
-                    j++;
-                 i++;
-            }
-            else
-            {
-                new_mas[i] = mas_1[k];
-                k--;
-                i++;
-            }
-            if (k < 0 && j<(size_2-1)) 
-            {
-                    do
-                    {
-                        new_mas[i] = mas_2[j];   
-                        j++;
-                        i++;
-                    } while (j < size_2 && i < new_size);
-            }
-            if (j > (size_2 - 1) && k > 0)
-            {
-                do {
-                    new_mas[i] = mas_1[k];
-                    i++;
-                    k--;
-                } while (k >= 0 && i < new_size);
-            }
+    input_mas(mas_dec, size_dec);
+    int new_size = size_inc + size_dec;
+    int new_mas[200];
+
+    for (int i = 0, k = size_inc - 1, j = 0; i < new_size;)
+    {
+
+        if (mas_dec[j] > mas_inc[k])
+        {
+            new_mas[i] = mas_dec[j];
+            if (j < size_dec)
+                j++;
+            i++;
         }
-        printf("\nNew decreasing array: \n ");
-    output_mas(new_mas,new_size);
+        else
+        {
+            new_mas[i] = mas_inc[k];
+            if (k >= 0)
+                k--;
+            i++;
+        }
+        if (k < 0 && j < (size_dec))
+        {
+            do
+            {
+                new_mas[i] = mas_dec[j];
+                j++;
+                i++;
+            } while (j <= size_dec && i < new_size);
+        }
+        if (j > (size_dec - 1) && k > 0)
+        {
+            do {
+                new_mas[i] = mas_inc[k];
+                i++;
+                k--;
+            } while (k >= 0 && i < new_size);
+        }
+    }
+    printf("\nNew decreasing array: \n ");
+    output_mas(new_mas, new_size);
     return 0;
 }
 
