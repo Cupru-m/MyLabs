a5175ad754efc3de0e2911a6dbc548ad0790b481
KuPruM <kuprum321@gmail.com>
2022-10-29 11:05:44 +0300

    Update Lab-3-task-1.cpp

--- Lab-3-task-1/Lab-3-task-1/Lab-3-task-1.cpp
+++ Lab-3-task-1/Lab-3-task-1/Lab-3-task-1.cpp
@@ -1,33 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+int random_input_mas(int* m, int size);
 int safe_scanf(int number);
 int input_mas(int* m, int size);
 int main()
 {
-    
-    int FirstPositiveInd=0, LastPositiveInd=0;
-   
-    int sum = 0, min;
-    int size=0;
+
+    int FirstPositiveInd = 0, LastPositiveInd = 0;
+
+    int sum = 0;
+    int size = 0;
     int mas[100];
     printf("Input the size of array:");
     size = safe_scanf(size);
-    while (size <=0 )
-    { 
+    while (size <= 0)
+    {
         printf("size must be >=0\n");
         rewind(stdin);
         size = safe_scanf(size);
     }
-    input_mas(mas, size);
-    min = mas[0];
-    for (int i = 0; i < size; i++)
-    {
-        if (mas[i] < min)
-        {
-            min = mas[i];
-        }
+    printf("Choose input method \n '1'-hand input \n '2'- random input\n");
+    int choise = 3;
+    choise = safe_scanf(choise);
+
+    while (choise < 0 || choise >2) {
+        printf("Involid input, try again ");
+        rewind(stdin);
+        choise = safe_scanf(choise);
     }
+    if (choise == 1) {
+        input_mas(mas, size);
+    }
+    else
+    {
+        random_input_mas(mas, size);
+    }
+
     int trigger = 0;
     for (int i = 0; i < size; i++)
     {
@@ -38,28 +49,26 @@ int main()
             break;
         }
     }
-        for (int i = size - 1; i > 0; i--)
-        {
-            if (mas[i] > 0 && mas[i] != mas[FirstPositiveInd])
-            {
-                LastPositiveInd = i;
-                trigger++;
-                break;
-            }
-        }
-        if (trigger < 2)
-        {
-            printf("The min value is %d\n", min);
-            printf("conditions are not satisfied");
-
-            return 0;
-        }
-    for (int i= FirstPositiveInd +1;i<LastPositiveInd;i++)
+    for (int i = size - 1; i > 0; i--)
     {
-            sum = sum+ mas[i];
+        if (mas[i] > 0 && mas[i] != mas[FirstPositiveInd])
+        {
+            LastPositiveInd = i;
+            trigger++;
+            break;
+        }
     }
-    printf("The min value is %d\n", min);
-    printf("The sum of numbers between the first and the last positive  values :%d",sum);
+    if (trigger < 2)
+    {
+        printf("conditions are not satisfied");
+
+        return 0;
+    }
+    for (int i = FirstPositiveInd + 1;i < LastPositiveInd;i++)
+    {
+        sum = sum + mas[i];
+    }
+    printf("The sum of numbers between the first and the last positive  values :%d", sum);
     return 0;
 }
 int input_mas(int* m, int size)
@@ -91,4 +100,14 @@ int safe_scanf(int number)
         res = scanf("%d", &number);
     }
     return number;
+}
+int random_input_mas(int* m, int size)
+{
+    srand(time(NULL));
+    for (int i = 0; i < size; i++)
+    {
+        m[i] = -100 + rand() % 200;
+        printf("The %d element = %d\n", i + 1, m[i]);
+    }
+    return *m;
 }
